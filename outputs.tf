5a5e81203b15cb73f23fa3f9dbe8bbd820be50e1
Joseph O'Deorain <jmodeorain14@hotmail.com>
2024-01-09 21:44:39 +0100

    refactor: update JMeter instance name references
    this commit updates JMeter instance name references from "slave" to
    "worker" for consistency in the project

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-# Output the  number of slave EC2 instances that have been created
-output "jmeter_slave_count" {
-  value = var.jmeter_slave_count
+# Output the number of JMeter worker EC2 instances that have been created
+output "jmeter_worker_count" {
+  value = var.jmeter_worker_count
 }
 
-# Output the private IP addresses of the JMeter slave EC2 instances
-output "jmeter_slave_private_ip_addresses" {
-  value = aws_instance.jmeter_slave.*.private_ip
+# Output the private IP addresses of the JMeter worker EC2 instances
+output "jmeter_worker_private_ip_addresses" {
+  value = aws_instance.jmeter_worker.*.private_ip
 }
 
 # Output the Grafana Cloud instance URL
